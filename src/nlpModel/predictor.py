e50eb40cca5f027e8d85b9e4cb41e5dc28c2574b
connor-li <77080536+connor-li@users.noreply.github.com>
2022-08-07 22:15:06 -0400

    machine learning connie

---
+++ src/nlpModel/predictor.py
@@ -0,0 +1,10 @@
+import pickle
+import os
+# import tensorflow as tf
+
+# dir = os.path.dirname(__file__)
+# rel_path = './model.pickle'
+# abs_file_path = os.path.join(dir, rel_path)
+# pickle_in = open(abs_file_path, 'rb')
+# pickle_model = pickle.load(pickle_in)
+# print(pickle_model.summary())
